6369a46a0b239d24a9a98d782b8ed0288cd637e9
Roman Kostin <36431458+romanvht@users.noreply.github.com>
2024-02-21 14:30:15 +1000

    Update and rename Range-Extraction to Range-Extraction.php

--- Range-Extraction.php
+++ Range-Extraction.php
@@ -10,30 +10,28 @@ solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 1
 // returns '-10--8,-6,-3-1,3-5,7-11,14,15,17-20'
 
 <?php
-
-  function solution(array $list): string {
-    $count = count($list);
-    $out = [];
-    $j = 0;
-  
-    for ($i = 0; $i < $count; $i = $j + 1) {
-      // Начало дапазона
-      $out[] = $list[$i];
+    function solution(array $list): string {
+        $count = count($list);
+        $out = [];
+        $j = 0;
       
-      // Конец диапазона
-      for ($j = $i + 1; $j < $count && $list[$j] == $list[$j-1] + 1; $j++);
-      $j--;
-      
-      if ($i == $j) {
-        $out[] = ",";
-      } else if ($i + 1 == $j) {
-        array_push($out, ",", $list[$j], ",");
-      } else {
-        array_push($out, "-", $list[$j], ",");
-      }
+        for ($i = 0; $i < $count; $i = $j + 1) {
+          // Начало дапазона
+          $out[] = $list[$i];
+          
+          // Конец диапазона
+          for ($j = $i + 1; $j < $count && $list[$j] == $list[$j-1] + 1; $j++);
+          $j--;
+          
+          if ($i == $j) {
+            $out[] = ",";
+          } else if ($i + 1 == $j) {
+            array_push($out, ",", $list[$j], ",");
+          } else {
+            array_push($out, "-", $list[$j], ",");
+          }
+        }
+        array_pop($out);
+        return implode($out);
     }
-    array_pop($out);
-    return implode($out);
-  }
-
 ?>
